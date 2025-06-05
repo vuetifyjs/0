9f70873ab4120fec34f5695eade80356df867db6
John Leider <john@vuetifyjs.com>
2025-06-04 21:03:00 -0500

    feat(Paper): add gradient support

--- src/components/V0Paper/V0Paper.vue
+++ src/components/V0Paper/V0Paper.vue
@@ -7,6 +7,7 @@
 <script setup lang="ts">
   import { useColor, type ColorProps } from '@/composables/color'
   import { useDimensions, type DimensionProps } from '@/composables/dimensions'
+  import { computed } from 'vue'
 
   export interface V0PaperProps extends ColorProps, DimensionProps {
     borderRadius?: string
@@ -18,6 +19,7 @@
     margin?: string | number
     opacity?: string | number
     padding?: string | number
+    gradient?: string
 
     tag?: string
   }
@@ -28,6 +30,21 @@
   const color = useColor(props.color ?? bgColor?.value, !props.color)
   const borderColor = useColor(props.borderColor ?? color?.value)
   const { dimensionStyles } = useDimensions(props, 'paper')
+
+  const gradient = computed(() => {
+    if (!props.gradient) return null
+    // gradient="linear-gradient(to right, #000000, #ffffff)"
+    if (props.gradient.includes('gradient(')) return props.gradient
+
+    const parts = props.gradient.split(' ')
+    // gradient="radial #ff0000 #00ff00"
+    const isRadial = parts.includes('radial')
+    // gradient="45deg #ff0000 #00ff00 #0000ff"
+    const direction = parts.find(p => p.includes('deg')) || (isRadial ? '' : 'to right')
+    const colors = parts.filter(p => p !== 'radial' && !p.includes('deg')).join(', ')
+
+    return isRadial ? `radial-gradient(${colors})` : `linear-gradient(${direction}, ${colors})`
+  })
 
   const classes = {
     'v0-paper': true,
@@ -45,6 +62,8 @@
     ['--v0-paper-padding']: props.padding,
     ['--v0-paper-opacity']: props.opacity,
     ['--v0-paper-margin']: props.margin,
+    ['--v0-paper-gradient']: gradient.value,
+
     ...dimensionStyles.value,
   }
 </script>
