efd8da5954b3381508aa2f4cbfe1cad90e7f4862
John Leider <john@vuetifyjs.com>
2025-06-04 21:13:59 -0500

    refactor(dimensions): update css variable names

--- src/composables/dimensions.ts
+++ src/composables/dimensions.ts
@@ -13,10 +13,10 @@ export function useDimensions (props: DimensionProps, name: string) {
   const dimensionStyles = shallowRef<DimensionProps>({
     [`--v0-${name}-width`]: props.width,
     [`--v0-${name}-height`]: props.height,
-    [`--v0-${name}-maxWidth`]: props.maxWidth,
-    [`--v0-${name}-maxHeight`]: props.maxHeight,
-    [`--v0-${name}-minWidth`]: props.minWidth,
-    [`--v0-${name}-minHeight`]: props.minHeight,
+    [`--v0-${name}-max-width`]: props.maxWidth,
+    [`--v0-${name}-max-height`]: props.maxHeight,
+    [`--v0-${name}-min-width`]: props.minWidth,
+    [`--v0-${name}-min-height`]: props.minHeight,
   })
 
   return { dimensionStyles }
