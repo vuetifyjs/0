63c923b1544e69e45b5d12587ceafb068050d2db
John Leider <john@vuetifyjs.com>
2025-06-15 15:14:08 -0500

    fix(color): fix reactivity bug

--- src/composables/color.ts
+++ src/composables/color.ts
@@ -96,10 +96,10 @@ export function useColor (
   props: ColorProps,
   name = getCurrentInstanceName(),
 ) {
-  const colorStyles = toRef(() => {
-    const bgColor = parseColor(props.bgColor)
-    const color = parseColor(props.color ?? bgColor?.value, !props.color)
+  const bgColor = parseColor(props.bgColor)
+  const color = parseColor(props.color ?? bgColor?.value, !props.color)
 
+  const colorStyles = toRef(() => {
     return {
       [`--v0-${name}-bg-color`]: bgColor?.value,
       [`--v0-${name}-color`]: color?.value,
