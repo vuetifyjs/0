3d38fda3730f0e5a5f9868b6f6184971bc3d44ae
John Leider <john@vuetifyjs.com>
2025-06-25 19:21:53 -0500

    refactor(Avatar): update exports

--- src/lib/Avatar/index.ts
+++ src/lib/Avatar/index.ts
@@ -1,3 +1,7 @@
+export * from './AvatarFallback.vue'
+export * from './AvatarImage.vue'
+export * from './AvatarRoot.vue'
+
 import Fallback from './AvatarFallback.vue'
 import Image from './AvatarImage.vue'
 import Root from './AvatarRoot.vue'
