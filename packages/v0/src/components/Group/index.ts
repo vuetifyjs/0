f24c06ef32538c17db98ee57e399db68e72c5575
John Leider <john@vuetifyjs.com>
2025-06-27 14:06:25 -0500

    chore(Group): clean up exports

--- packages/v0/src/components/Group/index.ts
+++ packages/v0/src/components/Group/index.ts
@@ -1,6 +1,7 @@
-export * from './GroupItem.vue'
+export type { GroupItemProps, GroupItemSlots } from './GroupItem.vue'
+export type { GroupRootProps, GroupRootSlots } from './GroupRoot.vue'
+
 import Item from './GroupItem.vue'
-export * from './GroupRoot.vue'
 import Root from './GroupRoot.vue'
 
 export const Group = {
