7751e365b2de8d51cc51479c79e9bc6b175b4994
John Leider <john@vuetifyjs.com>
2025-06-27 14:07:47 -0500

    chore(Step): update exports

--- packages/v0/src/components/Step/index.ts
+++ packages/v0/src/components/Step/index.ts
@@ -1,5 +1,5 @@
-export * from './StepItem.vue'
-export * from './StepRoot.vue'
+export type { StepItemProps, StepItemSlots } from './StepItem.vue'
+export type { StepRootProps, StepRootSlots } from './StepRoot.vue'
 
 import StepItem from './StepItem.vue'
 import StepRoot from './StepRoot.vue'
