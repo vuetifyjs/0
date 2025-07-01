fc348b9dc76d463f6eeff508fecf37f6ccc9d457
Henry <haviles04@gmail.com>
2025-06-30 20:10:23 -0400

    feat(composable/filter): added some and every mode

--- src/lib/composables/useFilter/index.ts
+++ src/lib/composables/useFilter/index.ts
@@ -4,26 +4,28 @@ import { computed, isRef } from 'vue'
 export type FilterQuery = string | Ref<string> | number | Ref<number> | boolean | Ref<boolean>
 export type FilterItem = string | number | boolean | Record<string, any>
 export type FilterFunction = (query: FilterQuery, item: FilterItem) => boolean
+export type FilterMode = 'every' | 'some'
 
 export interface UseFilterOptions {
   customFilter?: FilterFunction
   keys?: string[]
+  mode?: FilterMode
 }
 
 export interface UseFilterResult<T extends FilterItem = FilterItem> {
   items: ComputedRef<T[]>
 }
 
-function defaultFilter (query: FilterQuery, item: FilterItem, keys?: string[]): boolean {
+function defaultFilter (query: FilterQuery, item: FilterItem, keys?: string[], mode?: FilterMode): boolean {
   if (['string', 'number', 'boolean'].includes(typeof item)) {
     return String(item).toLowerCase().includes(String(query).toLowerCase())
   }
 
   if (typeof item === 'object' && item !== null) {
     const values = keys?.length ? keys.map(k => item[k]) : Object.values(item)
-    return values.some(value =>
-      String(value).toLowerCase().includes(String(query).toLowerCase()),
-    )
+    const matcher = (value: any) => String(value).toLowerCase().includes(String(query).toLowerCase())
+
+    return mode === 'every' ? values.every(val => matcher(val)) : values.some(val => matcher(val))
   }
 
   return false
@@ -34,8 +36,8 @@ export function useFilter<T extends FilterItem> (
   items: Ref<T[]> | T[],
   options: UseFilterOptions = {},
 ): UseFilterResult<T> {
-  const { customFilter, keys } = options
-  const filterFunction = customFilter ?? ((q, i) => defaultFilter(q, i, keys))
+  const { customFilter, keys, mode = 'some' } = options
+  const filterFunction = customFilter ?? ((q, i) => defaultFilter(q, i, keys, mode))
 
   const itemsRef = isRef(items) ? items : computed(() => items)
   const queryRef = isRef(query) ? query : computed(() => query)
