133d2ffdc469a896ee25ee2f8b48b02984cea8c3
Andrei Elkin <andreyyolkin@gmail.com>
2025-07-09 18:11:11 +0200

    build: add vitest config files for packages

---
+++ packages/0/vitest.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vitest/config'
+import { fileURLToPath } from 'node:url'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('src', import.meta.url)),
+      '@vuetify/0': fileURLToPath(new URL('src', import.meta.url)),
+      '@vuetify/paper': fileURLToPath(new URL('../paper/src', import.meta.url)),
+      // internal
+      '#v0': fileURLToPath(new URL('src', import.meta.url)),
+      '#paper': fileURLToPath(new URL('../paper/src', import.meta.url)),
+    },
+  },
+  test: {
+    projects: ['packages/*'],
+    environment: 'happy-dom',
+    globals: true,
+  },
+})
