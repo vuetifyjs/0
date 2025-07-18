e11db034305d695143da726506f0cf0cd4691fde
John Leider <john@vuetifyjs.com>
2025-07-17 23:56:31 -0500

    chore(useContext): clean-up

--- packages/0/src/composables/useContext/index.ts
+++ packages/0/src/composables/useContext/index.ts
@@ -4,6 +4,12 @@ import { inject, provide } from 'vue'
 // Types
 import type { App, InjectionKey } from 'vue'
 
+/**
+ * Simple composable that wraps Vue's `provide` and `inject` functions.
+ *
+ * @param key The key to provide or inject the context.
+ * @returns A tuple containing the inject function and a provide function.
+ */
 export function useContext<T> (key: InjectionKey<T> | string) {
   function provideContext (value: T, app?: App) {
     app ? app.provide(key, value) : provide(key, value)
@@ -13,7 +19,7 @@ export function useContext<T> (key: InjectionKey<T> | string) {
     const contextValue = inject<T>(key)
 
     if (contextValue === undefined) {
-      throw new Error(`Context "${String(key)}" not found. Ensure it's provided by an ancestor component.`)
+      throw new Error(`Context "${String(key)}" not found. Ensure it's provided by an ancestor.`)
     }
 
     return contextValue
