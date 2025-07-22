f1d8e02c7b6a981ccfc4fb806f95d48acb733457
John Leider <john@vuetifyjs.com>
2025-07-22 00:20:36 -0500

    chore(createContext): update doc blocks

--- packages/0/src/factories/createContext/index.ts
+++ packages/0/src/factories/createContext/index.ts
@@ -5,11 +5,10 @@ import { inject, provide } from 'vue'
 import type { App, InjectionKey } from 'vue'
 
 /**
- * Simple composable that provides a context for managing application state.
- *
- * @param key The key to provide or inject the context.
- * @template Z The type of the context.
- * @returns A function that retrieves the context value for the given key.
+ * A simple wrapper for tapping into a v0 namespace
+ * @param key The provided string or InjectionKey
+ * @template Z The type values for the context.
+ * @returns A function that retrieves context
  */
 export function useContext<Z> (key: InjectionKey<Z> | string) {
   return function (): Z {
@@ -19,16 +18,16 @@ export function useContext<Z> (key: InjectionKey<Z> | string) {
       throw new Error(`Context "${String(key)}" not found. Ensure it's provided by an ancestor.`)
     }
 
-    return contextValue as Z
+    return contextValue
   }
 }
 
 /**
- * Simple composable that wraps Vue's `provide` and `inject` functions.
- *
- * @param key The key to provide or inject the context.
- * @template Z The type of the context.
- * @returns A tuple containing the inject function and a provide function.
+ * A simple wrapper for Vues provide & inject systems
+ * to create context for managing application state
+ * @param key The provided string or InjectionKey
+ * @template Z The type values for the context.
+ * @returns A tuple containing provide/inject
  */
 export function createContext<Z> (key: InjectionKey<Z> | string) {
   function provideContext (value: Z, app?: App) {
