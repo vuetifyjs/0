65315d928dec25397c63d0d44e1dbec2f3546206
John Leider <9064066+johnleider@users.noreply.github.com>
2025-08-03 15:03:33 -0500

    feat(useLayout): add new composable (#18)
    Co-authored-by: Henry <haviles04@gmail.com>

---
+++ packages/0/src/composables/useLayout/index.ts
@@ -0,0 +1,119 @@
+// Composables
+import { useGroup } from '#v0/composables/useGroup'
+
+// Utilities
+import { computed, shallowReactive, shallowRef, onUnmounted, onMounted, getCurrentInstance } from 'vue'
+
+// Globals
+import { IN_BROWSER } from '#v0/constants/globals.ts'
+
+// Types
+import type { ComputedRef, ShallowReactive, ShallowRef } from 'vue'
+import type { GroupContext, GroupTicket } from '#v0/composables/useGroup'
+import type { ID } from '#v0/types'
+
+export type LayoutLocation = 'top' | 'bottom' | 'left' | 'right'
+
+export interface LayoutTicket extends GroupTicket {
+  order: number
+  position: LayoutLocation
+  size: number
+}
+
+export interface LayoutContext<Z extends LayoutTicket> extends GroupContext<Z> {
+  bounds: {
+    top: ComputedRef<number>
+    bottom: ComputedRef<number>
+    left: ComputedRef<number>
+    right: ComputedRef<number>
+  }
+  main: {
+    x: ComputedRef<number>
+    y: ComputedRef<number>
+    width: ComputedRef<number>
+    height: ComputedRef<number>
+  }
+  sizes: ShallowReactive<Map<ID, number>>
+  height: ShallowRef<number>
+  width: ShallowRef<number>
+}
+
+export function useLayout<
+  Z extends LayoutTicket = LayoutTicket,
+  E extends LayoutContext<Z> = LayoutContext<Z>,
+> (): E {
+  const registry = useGroup<Z, E>()
+
+  const sizes = shallowReactive(new Map<ID, number>())
+  const height = shallowRef(0)
+  const width = shallowRef(0)
+
+  const bounds = {
+    top: computed(() => sum('top')),
+    bottom: computed(() => sum('bottom')),
+    left: computed(() => sum('left')),
+    right: computed(() => sum('right')),
+  }
+
+  const main = {
+    x: computed(() => bounds.left.value),
+    y: computed(() => bounds.top.value),
+    width: computed(() => width.value - bounds.left.value - bounds.right.value),
+    height: computed(() => height.value - bounds.top.value - bounds.bottom.value),
+  }
+
+  function sum (position: LayoutLocation): number {
+    let total = 0
+    for (const item of registry.collection.values()) {
+      if (item.position === position) {
+        total += sizes.get(item.id) ?? item.size ?? 0
+      }
+    }
+    return total
+  }
+
+  function register (registrant: Partial<Z>): Z {
+    const item: Partial<Z> = {
+      position: registrant.position,
+      size: registrant.size,
+      order: registrant.order ?? 0,
+      ...registrant,
+    }
+
+    const ticket = registry.register(item)
+
+    sizes.set(ticket.id, ticket.size)
+
+    return ticket
+  }
+
+  if (IN_BROWSER && getCurrentInstance()) {
+    function resize () {
+      height.value = window.innerHeight
+      width.value = window.innerWidth
+    }
+
+    onMounted(() => {
+      resize()
+      window.addEventListener('resize', resize)
+    })
+
+    onUnmounted(() => {
+      window.removeEventListener('resize', resize)
+    })
+  }
+
+  registry.on('unregister', (item: Z) => {
+    sizes.delete(item.id)
+  })
+
+  return {
+    ...registry,
+    register,
+    bounds,
+    main,
+    sizes,
+    height,
+    width,
+  } as E
+}
