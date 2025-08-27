3cdb421866d7beece33a552d3fa9ab5b358074d2
Henry Aviles <haviles04@gmail.com>
2025-08-27 15:38:40 -0400

    fix: styling overflow

--- apps/docs/src/components/app/AppMain.vue
+++ apps/docs/src/components/app/AppMain.vue
@@ -7,14 +7,14 @@
 
 <template>
   <div
-    class="pa-4 fixed"
+    class="pa-4 fixed inset-0 overflow-y-auto "
     :style="{
       top: layout.main.y.value + 'px',
       width: layout.main.width.value + 'px',
       left: layout.main.x.value + 'px',
     }"
   >
-    <div class="max-w-[688px] mx-auto pb-4">
+    <div class="max-w-[688px] mx-auto pb-4 ">
       <router-view />
 
       <DocsBackmatter />
