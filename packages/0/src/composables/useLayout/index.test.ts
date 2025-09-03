f3c841f45b7462f734469e8c58439da57d0f90be
Henry Aviles <haviles04@gmail.com>
2025-09-03 09:39:13 -0400

    test: check child component

--- packages/0/src/composables/useLayout/index.test.ts
+++ packages/0/src/composables/useLayout/index.test.ts
@@ -1,7 +1,8 @@
 import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest'
 import { createLayout } from './index.ts'
 import { mount } from '@vue/test-utils'
-import { defineComponent, useTemplateRef } from 'vue'
+import { defineComponent, useTemplateRef, nextTick } from 'vue'
+import { Atom } from '@/components/Atom'
 
 describe('useLayout inside component', () => {
   let originalWindow: any
@@ -25,53 +26,53 @@ describe('useLayout inside component', () => {
   it('registers height and width in browser', () => {
     const testComponent = defineComponent({
       setup () {
-        const context = createLayout()[2]
-        return { context }
+        const layout = createLayout()[2]
+        return { layout }
       },
       template: '<div></div>',
     })
 
     const mountedComponent = mount(testComponent)
-    mountedComponent.vm.context.register({ id: 'Component1', position: 'top', value: 32 })
-    mountedComponent.vm.context.register({ id: 'Component2', position: 'left', value: 128 })
+    mountedComponent.vm.layout.register({ id: 'Component1', position: 'top', value: 32 })
+    mountedComponent.vm.layout.register({ id: 'Component2', position: 'left', value: 128 })
 
-    expect(mountedComponent.vm.context.width.value).toEqual(1024)
-    expect(mountedComponent.vm.context.height.value).toEqual(768)
+    expect(mountedComponent.vm.layout.width.value).toEqual(1024)
+    expect(mountedComponent.vm.layout.height.value).toEqual(768)
   })
 
   it('correctly calculates main', () => {
     const testComponent = defineComponent({
       setup () {
-        const context = createLayout()[2]
-        return { context }
+        const layout = createLayout()[2]
+        return { layout }
       },
       template: '<div></div>',
     })
 
     const mountedComponent = mount(testComponent)
 
-    mountedComponent.vm.context.register({ id: 'Component1', position: 'top', value: 32 })
-    mountedComponent.vm.context.register({ id: 'Component2', position: 'left', value: 128 })
+    mountedComponent.vm.layout.register({ id: 'Component1', position: 'top', value: 32 })
+    mountedComponent.vm.layout.register({ id: 'Component2', position: 'left', value: 128 })
 
-    expect(mountedComponent.vm.context.main.x.value).toEqual(128)
-    expect(mountedComponent.vm.context.main.y.value).toEqual(32)
-    expect(mountedComponent.vm.context.main.width.value).toEqual(896)
-    expect(mountedComponent.vm.context.main.height.value).toEqual(736)
+    expect(mountedComponent.vm.layout.main.x.value).toEqual(128)
+    expect(mountedComponent.vm.layout.main.y.value).toEqual(32)
+    expect(mountedComponent.vm.layout.main.width.value).toEqual(896)
+    expect(mountedComponent.vm.layout.main.height.value).toEqual(736)
   })
 
   it('correctly calculates size from component', async () => {
     const testComponent = defineComponent({
       setup () {
-        const context = createLayout()[2]
+        const layout = createLayout()[2]
         const element = useTemplateRef<HTMLElement>('element')
 
-        const item = context.register({
+        const item = layout.register({
           id: 'testdiv',
           position: 'top',
           element,
         })
 
-        return { item, context }
+        return { item, layout }
       },
       template: '<div id="testdiv" ref="element" style="height: 50px"></div> <div style="height: 60px"></div>',
     })
@@ -86,27 +87,58 @@ describe('useLayout inside component', () => {
     })
     const mountedComponent = mount(testComponent)
     expect(mountedComponent.vm.item.size.value).toEqual(50)
-    expect(mountedComponent.vm.context.main.y.value).toEqual(50)
+    expect(mountedComponent.vm.layout.main.y.value).toEqual(50)
+  })
+
+  it('correctly calculates size from child Atom component', async () => {
+    const testComponent = defineComponent({
+      components: { Atom },
+      setup () {
+        const layout = createLayout()[2]
+        const element = useTemplateRef<HTMLElement>('element')
+
+        const item = layout.register({
+          id: 'testdiv',
+          position: 'top',
+          element,
+        })
+
+        return { item, layout }
+      },
+      template: '<Atom ref="element" style="height:50px" id="atom"><div></div> </Atom>',
+    })
+
+    Object.defineProperty(HTMLElement.prototype, 'offsetHeight', {
+      configurable: true,
+      get () {
+        if (this.id === 'atom') {
+          return 50
+        }
+      },
+    })
+    const mountedComponent = mount(testComponent)
+    expect(mountedComponent.vm.item.size.value).toEqual(50)
+    expect(mountedComponent.vm.layout.main.y.value).toEqual(50)
   })
 })
 
 describe('useLayout outside component', () => {
   it('registers components', () => {
-    const context = createLayout()[2]
-    context.register({ id: 'Component1', position: 'top', value: 32 })
-    context.register({ id: 'Component2', position: 'bottom', value: 64 })
-    expect(context.collection.has('Component1')).toEqual(true)
-    expect(context.collection.has('Component2')).toEqual(true)
+    const layout = createLayout()[2]
+    layout.register({ id: 'Component1', position: 'top', value: 32 })
+    layout.register({ id: 'Component2', position: 'bottom', value: 64 })
+    expect(layout.collection.has('Component1')).toEqual(true)
+    expect(layout.collection.has('Component2')).toEqual(true)
   })
 
   it('calculates bound', () => {
-    const context = createLayout()[2]
-    context.register({ id: 'Component1', position: 'top', value: 32 })
-    context.register({ id: 'Component2', position: 'bottom', value: 128 })
-    context.register({ id: 'Component3', position: 'left', value: 64 })
+    const layout = createLayout()[2]
+    layout.register({ id: 'Component1', position: 'top', value: 32 })
+    layout.register({ id: 'Component2', position: 'bottom', value: 128 })
+    layout.register({ id: 'Component3', position: 'left', value: 64 })
 
-    expect(context.bounds.top.value).toEqual(32)
-    expect(context.bounds.bottom.value).toEqual(128)
-    expect(context.bounds.left.value).toEqual(64)
+    expect(layout.bounds.top.value).toEqual(32)
+    expect(layout.bounds.bottom.value).toEqual(128)
+    expect(layout.bounds.left.value).toEqual(64)
   })
 })
