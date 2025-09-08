04a7a35def9bf91ba2de7bfda8a1b4e743f73fd9
Henry Aviles <haviles04@gmail.com>
2025-09-08 10:18:22 -0400

    chore: dont nest ternary

--- packages/0/src/composables/useLayout/index.bench.ts
+++ packages/0/src/composables/useLayout/index.bench.ts
@@ -1,5 +1,5 @@
 import { createLayout } from '#v0'
-import { bench, describe, beforeEach } from 'vitest'
+import { bench, describe } from 'vitest'
 
 describe('useLayout benchmarks', () => {
   bench('register items', () => {
@@ -7,14 +7,14 @@ describe('useLayout benchmarks', () => {
     // Register 500 components
     for (let i = 0; i < 500; i++) {
       const order = i % 25 === 0 ? i * -1 : 0
-      const position =
-              i % 25 === 0
-                ? 'top'
-                : i % 30 === 0
-                  ? 'left'
-                  : i % 20 === 0
-                    ? 'bottom'
-                    : 'right'
+      function getPosition (i: number): 'top' | 'left' | 'bottom' | 'right' {
+        if (i % 25 === 0) return 'top'
+        if (i % 30 === 0) return 'left'
+        if (i % 20 === 0) return 'bottom'
+        return 'right'
+      }
+
+      const position = getPosition(i)
 
       layout.register({ id: `Component${i}`, position, value: i, order })
     }
@@ -25,14 +25,14 @@ describe('useLayout benchmarks', () => {
     // Register 500 components
     for (let i = 0; i < 500; i++) {
       const order = i % 25 === 0 ? i * -1 : 0
-      const position =
-                i % 25 === 0
-                  ? 'top'
-                  : i % 30 === 0
-                    ? 'left'
-                    : i % 20 === 0
-                      ? 'bottom'
-                      : 'right'
+      function getPosition (i: number): 'top' | 'left' | 'bottom' | 'right' {
+        if (i % 25 === 0) return 'top'
+        if (i % 30 === 0) return 'left'
+        if (i % 20 === 0) return 'bottom'
+        return 'right'
+      }
+
+      const position = getPosition(i)
 
       const item = layout.register({ id: `Component${i}`, position, value: i, order })
       const gotItem = layout.get(item.id)
