a212196ebf04bcfbb57a606c2b55f65dae925292
John Leider <john@vuetifyjs.com>
2025-10-22 22:42:39 -0500

    docs(AppNav): fix top location

--- apps/docs/src/components/app/AppNav.vue
+++ apps/docs/src/components/app/AppNav.vue
@@ -21,7 +21,7 @@
 <template>
   <Atom
     :as
-    class="bg-4 app-nav flex flex-col fixed w-[220px] overflow-y-auto py-4 top-[72px] bottom-[24px] translate-x-[-100%] md:top-[24px] md:bottom-0 md:translate-x-0"
+    class="bg-4 app-nav flex flex-col fixed w-[220px] overflow-y-auto py-4 top-[72px] bottom-[24px] translate-x-[-100%] md:bottom-0 md:translate-x-0"
     :class="app.drawer && '!translate-x-0'"
   >
     <ul class="flex gap-2 flex-col">
