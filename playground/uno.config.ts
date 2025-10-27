0d032cb85f613946bbe0908110cad17ea650fa0d
John Leider <john@vuetifyjs.com>
2025-10-27 11:38:39 -0500

    chore(playground): add uno css theme colors

--- playground/uno.config.ts
+++ playground/uno.config.ts
@@ -1,10 +1,10 @@
-import { presetWind4, defineConfig } from 'unocss'
+import { presetWind3, defineConfig } from 'unocss'
 
 const isStorybook = process.env.STORYBOOK === '1'
 
 export default defineConfig({
   presets: [
-    presetWind4(),
+    presetWind3(),
   ],
   content: {
     pipeline: {
@@ -13,6 +13,28 @@ export default defineConfig({
             './src/**/*.{vue,js,ts,jsx,tsx}',
           ]
         : [],
+    },
+  },
+  theme: {
+    colors: {
+      'primary': 'var(--v0-primary)',
+      'secondary': 'var(--v0-secondary)',
+      'accent': 'var(--v0-accent)',
+      'error': 'var(--v0-error)',
+      'info': 'var(--v0-info)',
+      'success': 'var(--v0-success)',
+      'warning': 'var(--v0-warning)',
+      'background': 'var(--v0-background)',
+      'surface': 'var(--v0-surface)',
+      'on-primary': 'var(--v0-on-primary)',
+      'on-secondary': 'var(--v0-on-secondary)',
+      'on-accent': 'var(--v0-on-accent)',
+      'on-error': 'var(--v0-on-error)',
+      'on-info': 'var(--v0-on-info)',
+      'on-success': 'var(--v0-on-success)',
+      'on-warning': 'var(--v0-on-warning)',
+      'on-background': 'var(--v0-on-background)',
+      'on-surface': 'var(--v0-on-surface)',
     },
   },
 })
