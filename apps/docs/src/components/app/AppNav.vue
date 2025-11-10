3454a6010658b381b54e53111e527bd2690bc017
John Leider <john@vuetifyjs.com>
2025-11-09 19:43:06 -0600

    docs(AppNav): remove custom styles

--- apps/docs/src/components/app/AppNav.vue
+++ apps/docs/src/components/app/AppNav.vue
@@ -1,8 +1,17 @@
 <script setup lang="ts">
+  // Vuetify0
   import { Atom, useBreakpoints } from '@vuetify/v0'
-  import { useAppStore } from '@/stores/app'
+
+  // Composables
   import { useRoute } from 'vue-router'
+
+  // Utilities
   import { watch } from 'vue'
+
+  // Stores
+  import { useAppStore } from '@/stores/app'
+
+  // Types
   import type { AtomProps } from '@vuetify/v0'
 
   const { as = 'nav' } = defineProps<AtomProps>()
@@ -21,7 +30,7 @@
 <template>
   <Atom
     :as
-    class="bg-4 app-nav flex flex-col fixed w-[220px] overflow-y-auto py-4 top-[72px] bottom-[24px] translate-x-[-100%] md:bottom-0 md:translate-x-0 transition-transform duration-200 ease-in-out"
+    class="bg-4 app-nav flex flex-col fixed w-[220px] overflow-y-auto py-4 top-[72px] bottom-[24px] translate-x-[-100%] md:bottom-0 md:translate-x-0 transition-transform duration-200 ease-in-out border-r border-solid border-divider"
     :class="app.drawer && '!translate-x-0'"
   >
     <ul class="flex gap-2 flex-col">
@@ -42,9 +51,3 @@
     </ul>
   </Atom>
 </template>
-
-<style lang="sass">
-  .app-nav
-    background-color: var(--v0-surface)
-    border-right: thin solid var(--v0-divider)
-</style>
