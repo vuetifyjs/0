aba5c6431e48c1bbc61d0c8b722cbaa2ad1bba36
John Leider <john@vuetifyjs.com>
2025-11-09 19:38:58 -0600

    docs(AppFooter): update style

--- apps/docs/src/components/app/AppFooter.vue
+++ apps/docs/src/components/app/AppFooter.vue
@@ -11,15 +11,10 @@
 <template>
   <Atom
     :as
-    class="app-footer flex items-center justify-between h-[24px] fixed bottom-0 left-0 md:left-[220px] right-0 px-3 text-xs bg-surface border-t"
+    class="app-footer flex items-center justify-between h-[24px] fixed bottom-0 left-0 md:left-[220px] right-0 px-3 text-xs bg-surface-tint text-on-surface-tint"
   >
     <DocsLastCommit />
 
-    &copy; 2016-2025 Vuetify, LLC
+    &copy; 2016-{{ (new Date()).getFullYear() }} Vuetify, LLC
   </Atom>
 </template>
-
-<style lang="sass">
-  .app-footer
-    background-color: var(--v0-surface-tint)
-</style>
