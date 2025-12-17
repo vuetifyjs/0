2d270f8e64e84944bc812b2d7d5011e546a67a10
John Leider <john@vuetifyjs.com>
2025-12-16 22:36:11 -0600

    docs: add storybook iframe page
    Embed Storybook at /storybook with home layout header/footer.

---
+++ apps/docs/src/pages/storybook.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+  //
+</script>
+
+<template>
+  <div class="storybook-page">
+    <iframe
+      class="storybook-iframe"
+      src="https://vuetifyjs.github.io/0/"
+      title="Vuetify 0 Storybook"
+    />
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
+
+<style scoped>
+  .storybook-page {
+    min-height: calc(100vh - 200px);
+  }
+
+  .storybook-iframe {
+    width: 100%;
+    height: calc(100vh - 200px);
+    border: none;
+  }
+</style>
