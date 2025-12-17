305b98ac3f2f0f614443029670c46d7841216bc3
John Leider <john@vuetifyjs.com>
2025-12-16 22:44:42 -0600

    fix(docs): use fullscreen layout for storybook page
    Use fullscreen layout and UnoCSS utilities for full
    width/height iframe.

--- apps/docs/src/pages/storybook.vue
+++ apps/docs/src/pages/storybook.vue
@@ -3,28 +3,14 @@
 </script>
 
 <template>
-  <div class="storybook-page">
-    <iframe
-      class="storybook-iframe"
-      src="https://vuetifyjs.github.io/0/"
-      title="Vuetify 0 Storybook"
-    />
-  </div>
+  <iframe
+    class="flex-1 w-full border-none"
+    src="https://vuetifyjs.github.io/0/"
+    title="Vuetify 0 Storybook"
+  />
 </template>
 
 <route lang="yaml">
 meta:
-  layout: home
+  layout: fullscreen
 </route>
-
-<style scoped>
-  .storybook-page {
-    min-height: calc(100vh - 200px);
-  }
-
-  .storybook-iframe {
-    width: 100%;
-    height: calc(100vh - 200px);
-    border: none;
-  }
-</style>
