12c9f8722a85b18f5045cdd2bcf13d92a67f45bb
John Leider <john@vuetifyjs.com>
2025-12-26 20:00:37 -0600

    refactor(docs): use global markdown-body class for AI responses

--- apps/docs/src/components/docs/DocsAskMessage.vue
+++ apps/docs/src/components/docs/DocsAskMessage.vue
@@ -70,7 +70,7 @@
     <div
       v-if="html"
       ref="content"
-      class="markdown-content"
+      class="markdown-body"
       v-html="html"
     />
 
@@ -99,70 +99,10 @@
   .delay-150 {
     animation-delay: 150ms;
   }
-
-  .markdown-content :deep(p) {
+  .markdown-body :deep(.docs-markup) {
     margin: 0.5em 0;
   }
-  .markdown-content :deep(p:first-child) {
-    margin-top: 0;
-  }
-  .markdown-content :deep(p:last-child) {
-    margin-bottom: 0;
-  }
-  .markdown-content :deep(code) {
-    background: var(--v0-surface);
-    padding: 0.125em 0.25em;
-    border-radius: 0.25em;
-    font-size: 0.875em;
-  }
-  .markdown-content :deep(pre) {
-    background: var(--v0-pre);
-    padding: 0.75em;
-    border-radius: 0.5em;
-    overflow-x: auto;
-    margin: 0.5em 0;
-  }
-  .markdown-content :deep(pre code) {
-    background: none;
-    padding: 0;
-  }
-  .markdown-content :deep(ul) {
-    margin: 0.5em 0;
-    padding-left: 1.5em;
-    list-style-type: disc;
-  }
-  .markdown-content :deep(ol) {
-    margin: 0.5em 0;
-    padding-left: 1.5em;
-    list-style-type: decimal;
-  }
-  .markdown-content :deep(li) {
-    margin: 0.25em 0;
-    display: list-item;
-  }
-  .markdown-content :deep(strong) {
-    font-weight: 600;
-  }
-  .markdown-content :deep(a) {
-    color: var(--v0-primary);
-    text-decoration: underline;
-  }
-  .markdown-content :deep(h1),
-  .markdown-content :deep(h2),
-  .markdown-content :deep(h3) {
-    font-weight: 600;
-    margin: 0.75em 0 0.5em;
-  }
-  .markdown-content :deep(blockquote) {
-    border-left: 3px solid var(--v0-divider);
-    padding-left: 1em;
-    margin: 0.5em 0;
-    opacity: 0.8;
-  }
-  .markdown-content :deep(.docs-markup) {
-    margin: 0.5em 0;
-  }
-  .markdown-content :deep(.docs-markup pre) {
+  .markdown-body :deep(.docs-markup pre) {
     padding-top: 2.5rem;
     padding-right: 5rem;
   }
