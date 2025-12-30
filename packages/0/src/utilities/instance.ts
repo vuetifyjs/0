ad82f0d14331340520772693b7f1c6c30a205f03
Andrei Elkin <andreyyolkin@gmail.com>
2025-12-30 01:50:52 +0100

    docs(instance): mark internal utility functions with @internal tag

--- packages/0/src/utilities/instance.ts
+++ packages/0/src/utilities/instance.ts
@@ -1,6 +1,7 @@
 // Utilities
 import * as Vue from 'vue'
 
+// @internal
 export function instanceExists () {
   if (Vue.version.startsWith('3.6.')) {
     // @ts-expect-error type is not defined in 3.6.0-beta.1
@@ -9,6 +10,7 @@ export function instanceExists () {
   return Vue.getCurrentInstance() !== null
 }
 
+// @internal
 export function instanceName () {
   if (Vue.version.startsWith('3.6.')) {
     // @ts-expect-error type is not defined in 3.6.0-beta.1
