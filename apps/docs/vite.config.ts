3dfe1b3b9c64bc0a41de561612a0262f8fd356a2
John Leider <john@vuetifyjs.com>
2025-12-29 19:01:54 -0600

    docs: reenable Vue Options API for plugin mixins
    The breakpoints and hydration plugins use mixins which require the Options API.
    With it disabled, the nav was stuck with `inert` attribute on desktop.

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -105,7 +105,7 @@ export default defineConfig({
     '__DEV__': process.env.NODE_ENV !== 'production',
     '__VERSION__': JSON.stringify(pkg.version),
     '__VITE_LOGGER_ENABLED__': process.env.VITE_LOGGER_ENABLED,
-    '__VUE_OPTIONS_API__': 'false',
+    '__VUE_OPTIONS_API__': 'true',
     '__VUE_PROD_DEVTOOLS__': 'false',
     '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': 'false',
   },
