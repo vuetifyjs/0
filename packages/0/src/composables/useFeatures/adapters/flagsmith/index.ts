c84da66f890eadc223761daedc866ed4184c8b71
Andrei Elkin <andreyyolkin@gmail.com>
2026-01-22 01:08:19 +0100

    refactor(flagsmith): rename flagsmith instance variable to client for clarity

--- packages/0/src/composables/useFeatures/adapters/flagsmith/index.ts
+++ packages/0/src/composables/useFeatures/adapters/flagsmith/index.ts
@@ -5,19 +5,19 @@ import type { FeaturesAdapterFlags, FeaturesAdapterInterface } from '../generic'
 import type { IFlagsmith, IInitConfig } from 'flagsmith'
 
 export class FlagsmithFeatureAdapter implements FeaturesAdapterInterface {
-  private flagsmith: IFlagsmith
+  private client: IFlagsmith
   private options: IInitConfig
 
-  constructor (flagsmithInstance: IFlagsmith = flagsmith, options: IInitConfig) {
-    this.flagsmith = flagsmithInstance
+  constructor (client: IFlagsmith = flagsmith, options: IInitConfig) {
+    this.client = client
     this.options = options
   }
 
   setup (onUpdate: (flags: FeaturesAdapterFlags) => void): FeaturesAdapterFlags {
-    this.flagsmith.init({
+    this.client.init({
       ...this.options,
       onChange: (oldFlags: any, params: any, loadingState: any) => {
-        const flags = this.flagsmith.getAllFlags()
+        const flags = this.client.getAllFlags()
         const adapterFlags: FeaturesAdapterFlags = {}
 
         for (const [key, flag] of Object.entries(flags) as any) {
@@ -35,7 +35,7 @@ export class FlagsmithFeatureAdapter implements FeaturesAdapterInterface {
       },
     })
 
-    this.disposeFn = () => this.flagsmith.stopListening()
+    this.disposeFn = () => this.client.stopListening()
 
     return {}
   }
