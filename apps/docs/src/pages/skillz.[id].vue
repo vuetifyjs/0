72aef6cf4d15d46c55337cfb87f7bfa8939b859c
John Leider <john@vuetifyjs.com>
2026-03-11 12:29:18 -0500

    docs: fix skillz pages overlapped by appbar and banner
    Add pt-[72px] to fullscreen layout to match default layout,
    and correct stale 100vh-64px to 100vh-72px in skillz detail page.

--- apps/docs/src/pages/skillz.[id].vue
+++ apps/docs/src/pages/skillz.[id].vue
@@ -98,7 +98,7 @@
 </script>
 
 <template>
-  <div class="min-h-[calc(100vh-64px)]">
+  <div class="min-h-[calc(100vh-72px)]">
     <!-- Not found state -->
     <div v-if="!tour" class="flex flex-col items-center justify-center py-16 px-8 text-center">
       <h1 class="m-0 mb-2 text-2xl">Skill Not Found</h1>
