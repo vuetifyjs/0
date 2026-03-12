205c7faf39b7c7a2025cc9c73bbfb1eaa0ed96be
John Leider <john@vuetifyjs.com>
2026-03-12 14:22:22 -0500

    chore: fix knip issues and add remaining docs examples

---
+++ apps/docs/src/examples/components/slider/pill-slider.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+  import PillSlider from './PillSlider.vue'
+  import { shallowRef } from 'vue'
+
+  const value = shallowRef(40)
+</script>
+
+<template>
+  <div class="flex flex-col gap-6 p-4">
+    <div>
+      <div class="text-sm text-on-surface-variant mb-1">
+        Value: <span class="font-mono">{{ value }}</span>
+      </div>
+
+      <PillSlider v-model="value" />
+    </div>
+
+    <div class="flex gap-4">
+      <button
+        v-for="v in [0, 25, 50, 75, 100]"
+        :key="v"
+        class="px-3 py-1 text-sm rounded-full border border-outline-variant hover:bg-surface-variant transition-colors"
+        @click="value = v"
+      >
+        {{ v }}%
+      </button>
+    </div>
+  </div>
+</template>
