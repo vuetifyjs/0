1f68fd3dc629da8e0f11e773f071ee03063eb7b6
John Leider <john@vuetifyjs.com>
2026-03-25 15:14:38 -0500

    feat(emerald): implement EmButton reference component
    First reference component establishing Paper DS conventions:
    - Root with V0Paper, sub-components as plain elements
    - Props forwarded to V0Paper via v-bind spread
    - Data attributes for state styling (disabled, loading)
    - Explicit event re-exposure, no $attrs forwarding
    - Sub-components: EmButtonContent, EmButtonPrepend,
      EmButtonAppend, EmButtonLoader

---
+++ packages/emerald/src/components/EmButton/EmButtonLoader.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export interface EmButtonLoaderProps {}
+</script>
+
+<script setup lang="ts">
+  defineOptions({ name: 'EmButtonLoader' })
+</script>
+
+<template>
+  <span aria-hidden="true" class="emerald-button__loader">
+    <slot />
+  </span>
+</template>
--- packages/emerald/src/components/EmButton/index.ts
+++ packages/emerald/src/components/EmButton/index.ts
@@ -1,2 +1,14 @@
-export { default as EmButton } from './EmButton.vue'
 export type { EmButtonProps } from './EmButton.vue'
+export { default as EmButton } from './EmButton.vue'
+
+export type { EmButtonAppendProps } from './EmButtonAppend.vue'
+export { default as EmButtonAppend } from './EmButtonAppend.vue'
+
+export type { EmButtonContentProps } from './EmButtonContent.vue'
+export { default as EmButtonContent } from './EmButtonContent.vue'
+
+export type { EmButtonLoaderProps } from './EmButtonLoader.vue'
+export { default as EmButtonLoader } from './EmButtonLoader.vue'
+
+export type { EmButtonPrependProps } from './EmButtonPrepend.vue'
+export { default as EmButtonPrepend } from './EmButtonPrepend.vue'
