23b166ac5b05ae8bc0119d35e1845c32feb391a8
John Leider <john@vuetifyjs.com>
2026-03-29 13:28:09 -0500

    fix(helix): add component auto-resolver to dev vite config

--- packages/helix/dev/vite.config.ts
+++ packages/helix/dev/vite.config.ts
@@ -1,11 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import UnoCSS from 'unocss/vite'
+import Components from 'unplugin-vue-components/vite'
 import Vue from 'unplugin-vue/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [Vue(), UnoCSS()],
+  plugins: [
+    Vue(),
+    Components({
+      dirs: [],
+      resolvers: [
+        name => {
+          if (name.startsWith('Hx')) {
+            return { name, from: fileURLToPath(new URL('../src', import.meta.url)) }
+          }
+        },
+      ],
+    }),
+    UnoCSS(),
+  ],
   resolve: {
     alias: {
       '#helix': fileURLToPath(new URL('../src', import.meta.url)),
