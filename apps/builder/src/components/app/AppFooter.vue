96a4ed2c7965090c63ac2ee2530ae0bd367c2202
John Leider <john@vuetifyjs.com>
2026-04-04 17:46:52 -0500

    feat(builder): add copyright footer

---
+++ apps/builder/src/components/app/AppFooter.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <footer class="py-6 text-center text-xs text-on-surface-variant/60">
+    &copy; 2016-{{ new Date().getFullYear() }} Vuetify, LLC
+  </footer>
+</template>
