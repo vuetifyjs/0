384592b767515977ead660c6fe279a4bb11639ba
John Leider <john@vuetifyjs.com>
2026-04-05 17:31:55 -0500

    chore(Popover): remove broken anchor fragments from @see URLs

--- packages/0/src/components/Popover/index.ts
+++ packages/0/src/components/Popover/index.ts
@@ -45,7 +45,7 @@ export const Popover = {
   /**
    * Activator component that opens the popover.
    *
-   * @see https://0.vuetifyjs.com/components/disclosure/popover#popoveractivator
+   * @see https://0.vuetifyjs.com/components/disclosure/popover
    *
    * @example
    * ```vue
@@ -64,7 +64,7 @@ export const Popover = {
   /**
    * Component for the content section of a popover.
    *
-   * @see https://0.vuetifyjs.com/components/disclosure/popover#popovercontent
+   * @see https://0.vuetifyjs.com/components/disclosure/popover
    *
    * @example
    * ```vue
