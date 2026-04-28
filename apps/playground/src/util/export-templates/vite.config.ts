851df73246df2279209f27357c44b5b95fd661f7
John Leider <john@vuetifyjs.com>
2026-04-27 22:59:21 -0500

    chore(playground): point UnoCSS plugin at src config in export

--- apps/playground/src/util/export-templates/vite.config.ts
+++ apps/playground/src/util/export-templates/vite.config.ts
@@ -5,7 +5,7 @@ import UnoCSS from 'unocss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [Vue(), UnoCSS()],
+  plugins: [Vue(), UnoCSS({ configFile: './src/uno.config.ts' })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('src', import.meta.url)),
