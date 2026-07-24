c4f7b54dfa79f9c21864c5a565fa8a94bec247a2
John Leider <john@vuetifyjs.com>
2026-07-24 16:31:36 -0500

    chore(dev): wire Emerald Wave 1 kitchen sink at /emerald

--- dev/src/main.ts
+++ dev/src/main.ts
@@ -3,6 +3,7 @@ import { ViteSSG } from 'vite-ssg'
 // Context
 import App from './App.vue'
 import CreateOverflowDemo from './CreateOverflowDemo.vue'
+import EmeraldKitchenSink from './EmeraldKitchenSink.vue'
 import Playground from './Playground.vue'
 
 import 'virtual:uno.css'
@@ -15,6 +16,7 @@ export const createApp = ViteSSG(
     routes: [
       { path: '/', component: Playground },
       { path: '/create-overflow', component: CreateOverflowDemo },
+      { path: '/emerald', component: EmeraldKitchenSink },
     ],
   },
   async ({ app }) => {
